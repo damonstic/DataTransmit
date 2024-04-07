2d3baa1687ab3319e9159cce6e593c3755a6233b
damonstic <2509575042@qq.com>
2024-04-07 16:23:46 +0800

    commit_0407

---
+++ src/main/java/com/csot/kafka/HttpPostSenderScope.java
@@ -0,0 +1,95 @@
+package com.csot.kafka;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+@Component
+public class HttpPostSenderScope {
+    private final static Logger logger = LoggerFactory.getLogger(HttpPostSenderScope.class);
+    private final RestTemplate restTemplate;
+
+    @Value("${endpoint.url}")
+    private String endpointUrl;
+
+    @Autowired
+    public HttpPostSenderScope(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public void uploadHandCheckFile(String filePath) throws IOException {
+        try {
+            // 读取文件内容
+            JSONArray jsonArray = readJsonFile(filePath);
+
+            // 如果 JSON 数据不为空，则发送数据
+            if (jsonArray != null && !jsonArray.isEmpty()) {
+                send("scope", jsonArray);
+                logger.info("发送scope");
+                // 清空文件内容或删除文件
+                clearJsonFile(filePath);
+            }
+        } catch (IOException e) {
+            logger.error("Error reading JSON file: " + e.getMessage());
+        }
+    }
+
+
+    // 读取 JSON 文件内容的方法
+    private JSONArray readJsonFile(String filePath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            StringBuilder jsonData = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonData.append(line);
+            }
+            return JSON.parseArray(jsonData.toString());
+        }
+    }
+
+    private void clearJsonFile(String filePath) throws IOException {
+        try (FileWriter fileWriter = new FileWriter(filePath)) {
+            // 清空文件内容
+            fileWriter.write("");
+        } catch (IOException e) {
+            // 处理异常
+            e.printStackTrace();
+        }
+    }
+
+
+    public void send(String dataType,JSONArray inputData) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8); //创建HTTP头部信息
+
+        //传参
+        JSONObject requestData = new JSONObject();
+        requestData.put("forecast_length", "24");
+        requestData.put("interval", "10min");
+        requestData.put("train_test_cutoff", "2024-04-03 00:00:00.000");
+        requestData.put("n_training_days", "2");
+        requestData.put("upper_limit", "0.95");
+        requestData.put("lower_limit", "0.05");
+        requestData.put("data_type", dataType);
+        requestData.put("input_data", inputData);
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(requestData.toString(), headers);//创建HTTP实体对象
+        ResponseEntity<String> response = restTemplate.exchange(endpointUrl, HttpMethod.POST, requestEntity, String.class);//发送HTTP请求
+        logger.info("HTTP Status Code: " + response.getStatusCode()); //处理HTTP响应
+        logger.info("Response Body: " + response.getBody());
+    }
+
+}
--- src/main/java/com/csot/kafka/ScopedataConsumer.java
+++ src/main/java/com/csot/kafka/ScopedataConsumer.java
@@ -1,25 +1,72 @@
 package com.csot.kafka;
 
-import com.alibaba.fastjson2.JSONObject;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.MessagingException;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
 
 @Component
 public class ScopedataConsumer {
+
+    @Value("${json.file.path_scope01}")
+    private String jsonFilePath01;
+    @Value("${json.file.path_scope02}")
+    private String jsonFilePath02;
     @Autowired
-    private HttpPostSender httpPostSender;
-    @KafkaListener(topics = {"scopedatanew"},groupId = "cassandranew")
-    public void listen(List<String> scopes) throws MessagingException, ParseException, JsonProcessingException {
-        for (String scope : scopes) {
-            JSONObject json = JSONObject.parseObject(scope);
-            httpPostSender.send(json);
+    private HttpPostSenderScope httpPostSender;
+    // 记录当前应该存储到哪个文件的标记
+    private boolean writeToFirstFile = true;
+    @KafkaListener(topics = {"scopedata"},groupId = "cassandranew")
+    public void listen(List<String> scopes) throws MessagingException,ParseException{
+        // 根据标记判断存储文件路径
+        String jsonFilePath = writeToFirstFile ? jsonFilePath01 : jsonFilePath02;
+        saveDataToJson(scopes, jsonFilePath);
+        // 文件写入完成后通知HttpPostSender上传文件
+        try {
+            httpPostSender.uploadHandCheckFile(jsonFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
+        // 切换标记，以便下一次存储到另一个文件
+        writeToFirstFile = !writeToFirstFile;
+
     }
+
+    private void saveDataToJson(List<String> data, String filePath) {
+        // 创建 JSON 数组字符串
+        StringBuilder jsonArrayBuilder = new StringBuilder();
+        jsonArrayBuilder.append("[");
+        for (String hand : data) {
+            System.out.println("scope传输");
+            // 假设每条数据都是一个 JSON 对象字符串
+            jsonArrayBuilder.append(hand).append(",");
+        }
+        if (!data.isEmpty()) {
+            jsonArrayBuilder.deleteCharAt(jsonArrayBuilder.length() - 1); // 删除最后一个逗号
+        }
+        jsonArrayBuilder.append("]");
+
+        // 将 JSON 数组字符串写入文件
+        try (FileWriter fileWriter = new FileWriter(filePath, true)) {
+            fileWriter.write(jsonArrayBuilder.toString());
+            fileWriter.write("\n"); // 换行
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 每隔10分钟执行一次定时任务，用于切换存储文件路径
+    @Scheduled(fixedDelay = 60000)
+    public void switchFilePath() {
+        writeToFirstFile = !writeToFirstFile;
+    }
+
 }
